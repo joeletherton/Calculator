09496b47f2514ccc41a41b312db8352086c1f0f4
Joel Etherton <joel.etherton@gmail.com>
2014-05-22 11:37:44 -0400

    Adding calculator solution to repository
    Initial commit

---
+++ Calculator/Calculator.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCurrentValue = new System.Windows.Forms.TextBox();
+            this.btnMemoryClear = new System.Windows.Forms.Button();
+            this.btnMemorySubtract = new System.Windows.Forms.Button();
+            this.btnMemoryAdd = new System.Windows.Forms.Button();
+            this.btnMemoryRecall = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnDecimal = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.lblMemorySet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCurrentValue
+            // 
+            this.txtCurrentValue.BackColor = System.Drawing.SystemColors.Info;
+            this.txtCurrentValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCurrentValue.Font = new System.Drawing.Font("Century Gothic", 60F);
+            this.txtCurrentValue.Location = new System.Drawing.Point(14, 13);
+            this.txtCurrentValue.Margin = new System.Windows.Forms.Padding(5);
+            this.txtCurrentValue.MaxLength = 8;
+            this.txtCurrentValue.Name = "txtCurrentValue";
+            this.txtCurrentValue.Size = new System.Drawing.Size(416, 99);
+            this.txtCurrentValue.TabIndex = 1;
+            this.txtCurrentValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnMemoryClear
+            // 
+            this.btnMemoryClear.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMemoryClear.Location = new System.Drawing.Point(12, 121);
+            this.btnMemoryClear.Name = "btnMemoryClear";
+            this.btnMemoryClear.Size = new System.Drawing.Size(100, 100);
+            this.btnMemoryClear.TabIndex = 2;
+            this.btnMemoryClear.Text = "MC";
+            this.btnMemoryClear.UseVisualStyleBackColor = true;
+            this.btnMemoryClear.Click += new System.EventHandler(this.btnMemoryClear_Click);
+            // 
+            // btnMemorySubtract
+            // 
+            this.btnMemorySubtract.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMemorySubtract.Location = new System.Drawing.Point(330, 121);
+            this.btnMemorySubtract.Name = "btnMemorySubtract";
+            this.btnMemorySubtract.Size = new System.Drawing.Size(100, 100);
+            this.btnMemorySubtract.TabIndex = 4;
+            this.btnMemorySubtract.Text = "M-";
+            this.btnMemorySubtract.UseVisualStyleBackColor = true;
+            // 
+            // btnMemoryAdd
+            // 
+            this.btnMemoryAdd.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMemoryAdd.Location = new System.Drawing.Point(224, 121);
+            this.btnMemoryAdd.Name = "btnMemoryAdd";
+            this.btnMemoryAdd.Size = new System.Drawing.Size(100, 100);
+            this.btnMemoryAdd.TabIndex = 5;
+            this.btnMemoryAdd.Text = "M+";
+            this.btnMemoryAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnMemoryRecall
+            // 
+            this.btnMemoryRecall.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMemoryRecall.Location = new System.Drawing.Point(118, 121);
+            this.btnMemoryRecall.Name = "btnMemoryRecall";
+            this.btnMemoryRecall.Size = new System.Drawing.Size(100, 100);
+            this.btnMemoryRecall.TabIndex = 6;
+            this.btnMemoryRecall.Text = "MR";
+            this.btnMemoryRecall.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(118, 227);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 100);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "DEL";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(224, 227);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(100, 100);
+            this.btnDivide.TabIndex = 9;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(330, 227);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(100, 100);
+            this.btnMultiply.TabIndex = 8;
+            this.btnMultiply.Text = "X";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(12, 227);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 100);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(119, 331);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(100, 100);
+            this.btn8.TabIndex = 18;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(225, 331);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(100, 100);
+            this.btn9.TabIndex = 17;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(330, 333);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(100, 100);
+            this.btnSubtract.TabIndex = 16;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(13, 331);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(100, 100);
+            this.btn7.TabIndex = 15;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(118, 439);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(100, 100);
+            this.btn5.TabIndex = 22;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(224, 439);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(100, 100);
+            this.btn6.TabIndex = 21;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(330, 439);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 100);
+            this.btnAdd.TabIndex = 20;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 439);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(100, 100);
+            this.btn4.TabIndex = 19;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(118, 545);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 100);
+            this.btn2.TabIndex = 26;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(224, 545);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(100, 100);
+            this.btn3.TabIndex = 25;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEquals.Location = new System.Drawing.Point(330, 545);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(100, 205);
+            this.btnEquals.TabIndex = 24;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 545);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 100);
+            this.btn1.TabIndex = 23;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btnDecimal
+            // 
+            this.btnDecimal.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecimal.Location = new System.Drawing.Point(224, 650);
+            this.btnDecimal.Name = "btnDecimal";
+            this.btnDecimal.Size = new System.Drawing.Size(100, 100);
+            this.btnDecimal.TabIndex = 29;
+            this.btnDecimal.Text = ".";
+            this.btnDecimal.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(12, 650);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(206, 100);
+            this.btn0.TabIndex = 27;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // lblMemorySet
+            // 
+            this.lblMemorySet.AutoSize = true;
+            this.lblMemorySet.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblMemorySet.Location = new System.Drawing.Point(18, 88);
+            this.lblMemorySet.Name = "lblMemorySet";
+            this.lblMemorySet.Size = new System.Drawing.Size(25, 21);
+            this.lblMemorySet.TabIndex = 30;
+            this.lblMemorySet.Text = "M";
+            this.lblMemorySet.Visible = false;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(444, 762);
+            this.Controls.Add(this.lblMemorySet);
+            this.Controls.Add(this.btnDecimal);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnMemoryRecall);
+            this.Controls.Add(this.btnMemoryAdd);
+            this.Controls.Add(this.btnMemorySubtract);
+            this.Controls.Add(this.btnMemoryClear);
+            this.Controls.Add(this.txtCurrentValue);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCurrentValue;
+        private System.Windows.Forms.Button btnMemoryClear;
+        private System.Windows.Forms.Button btnMemorySubtract;
+        private System.Windows.Forms.Button btnMemoryAdd;
+        private System.Windows.Forms.Button btnMemoryRecall;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnDecimal;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Label lblMemorySet;
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        private const string EQUALS_OP = "Equals";
+        private const string SUBTRACT_OP = "Subtract";
+        private const decimal EPSILON = 0.0000000001M;
+
+        public Calculator()
+        {
+            InitializeComponent();
+
+            // Establish events
+            // Some of these probably could have been
+            // done using the designer, but what's the
+            // fun in doing that?
+            btnClear.Click += btnClear_Click;
+            btn0.Click += btnNumeric_Add;
+            btn1.Click += btnNumeric_Add;
+            btn2.Click += btnNumeric_Add;
+            btn3.Click += btnNumeric_Add;
+            btn4.Click += btnNumeric_Add;
+            btn5.Click += btnNumeric_Add;
+            btn6.Click += btnNumeric_Add;
+            btn7.Click += btnNumeric_Add;
+            btn8.Click += btnNumeric_Add;
+            btn9.Click += btnNumeric_Add;
+            btnDecimal.Click += btnNumeric_Add;
+            btnDelete.Click += btnDelete_Click;
+            btnAdd.Click += btnOperation_Add;
+            btnSubtract.Click += btnOperation_Add;
+            btnMultiply.Click += btnOperation_Add;
+            btnDivide.Click += btnOperation_Add;
+            btnEquals.Click += btnOperation_Add;
+            btnMemoryClear.Click += btnMemoryClear_Click;
+            btnMemoryRecall.Click += btnMemoryRecall_Click;
+            btnMemoryAdd.Click += btnMemory_Operation;
+            btnMemorySubtract.Click += btnMemory_Operation;
+            txtCurrentValue.KeyPress += txtCurrentValue_KeyPress;
+        }
+
+        private void txtCurrentValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Does not allow a keypress if the keypress is not a control, digit or decimal
+            // and disallows multiple decimals
+            e.Handled =
+                (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+                ||
+                (e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') >= 0);
+        }
+
+        private void btnNumeric_Add(object sender, EventArgs e)
+        {
+            if (txtCurrentValue.Text.Length >= 9)
+            {
+                MessageBox.Show(
+                    "Numbers may not exceed 9 digits", 
+                    "Max length problem", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Hand);
+                return;
+            }
+
+            var valueToAdd = ((Button)sender).Text.Trim();
+
+            if (StartNewEntry || Math.Abs(GetDecimalValue()) < EPSILON)
+                txtCurrentValue.Text = "";
+
+            txtCurrentValue.Text += valueToAdd;
+
+            StartNewEntry = false;
+            OperationNewlySet = false;
+        }
+
+        private void btnOperation_Add(object sender, EventArgs e)
+        {
+            // These are needed to set the proper operation
+            var operation = ((Button)sender).Name.Substring(3);
+
+            // If the operation is being clicked again without 
+            // first having another number input, e.g. someone
+            // clicks add then clicks multiply, this will be
+            // a replacement operation
+            if (OperationNewlySet)
+            {
+                ActiveOperation = operation;
+                return;
+            }
+
+            var value = GetDecimalValue();
+            
+            // The selected operation to compute was already clicked
+            // in the last event. We need to complete that operation
+            // before moving on to the next one
+            switch (ActiveOperation)
+            {
+                case "Add":
+                    RunningValue += value;
+                    break;
+                case "Subtract":
+                    RunningValue -= value;
+                    break;
+                case "Multiply":
+                    RunningValue *= value;
+                    break;
+                case "Divide":
+                    RunningValue /= value;
+                    break;
+                default:
+                    RunningValue = value;
+                    break;
+            }
+
+            txtCurrentValue.Text = RunningValue.ToString();
+
+            // At this point, we need to reset
+            // the active operation to the newly selected
+            // operation
+            if (!EQUALS_OP.Equals(operation, StringComparison.OrdinalIgnoreCase))
+            {
+                ActiveOperation = operation;
+                OperationNewlySet = true;
+            }
+            else
+            {
+                ActiveOperation = string.Empty;
+            }
+
+            // We set StartNewEntry so that the current value 
+            // textbox is cleared on the next keystroke
+            StartNewEntry = true;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtCurrentValue.Text))
+            {
+                txtCurrentValue.Text =
+                    txtCurrentValue.Text.Substring(0, txtCurrentValue.Text.Length - 1);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCurrentValue.Text = "";
+            RunningValue = 0;
+            StartNewEntry = true;
+            OperationNewlySet = false;
+        }
+
+        private void btnMemoryClear_Click(object sender, EventArgs e)
+        {
+            Memory = 0;
+            lblMemorySet.Visible = false;
+        }
+
+        private void btnMemoryRecall_Click(object sender, EventArgs e)
+        {
+            txtCurrentValue.Text = Memory.ToString();
+            StartNewEntry = true;
+        }
+
+        private void btnMemory_Operation(object sender, EventArgs e)
+        {
+            var value = GetDecimalValue();
+
+            var btn = (Button)sender;
+            if (btn.Name.Contains(SUBTRACT_OP))
+                value *= -1;
+
+            Memory += value; // Subtracts will be negative
+            lblMemorySet.Visible = true;
+            StartNewEntry = true;
+        }
+
+        private decimal GetDecimalValue()
+        {
+            decimal value;
+            decimal.TryParse(txtCurrentValue.Text, out value);
+
+            return value;
+        }
+
+        public decimal Memory { get; set; }
+        public bool StartNewEntry { get; set; }
+        public decimal RunningValue { get; set; }
+        public string ActiveOperation { get; set; }
+        public bool OperationNewlySet { get; set; }
+    }
+}
